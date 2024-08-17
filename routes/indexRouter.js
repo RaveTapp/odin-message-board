e9ea995ed395d7a1b570e8625e022262178a871a
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-08-17 10:47:11 +0200

    Add new message form

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -18,5 +18,9 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+})
+
 
 module.exports = indexRouter;
