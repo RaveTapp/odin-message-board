5e46bce05a3fd3763441c45879ab9a278b90c085
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2024-08-17 09:25:07 +0200

    Add basic message display

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 
+const indexRouter = require("./routes/indexRouter");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
-app.get("/", (req, res) => {res.render("index")});
+app.use("/", indexRouter);
 
 app.get("/new", (req, res) => {res.render("new")})
 
